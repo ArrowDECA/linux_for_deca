d215f6a48bca7a55883a0c7a00a77fd18d51a25e
Martin Dubois <mdubois@Ca.(none)>
2015-10-30 14:38:38 -0400

    Clean env scripts

--- env/nios2_create_hex.sh
+++ env/nios2_create_hex.sh
@@ -3,10 +3,12 @@
 # Author	:	KMS	-	Martin Dubois, ing.
 # Client	:	Coveloz
 # Product	:	linux-socfpga
-# Fichier	:	env/create_hex.sh
-# Usage		:	./env/create_hex.sh
+# Fichier	:	env/nios2_create_hex.sh
+# Usage		:	./env/nios2_create_hex.sh
 
-ALTERA=~/altera/15.0
+echo  Executing env/nios2_create_hex.sh
+
+ALTERA=/altera/15.0
 NIOS=$ALTERA/nios2eds
 NIOS_BIN=$NIOS/bin
 NIOS_BIN_GNU=$NIOS_BIN/gnu/H-x86_64-pc-linux-gnu/bin
@@ -15,7 +17,18 @@ NIOS_BOOT=$NIOS/components/altera_nios2/boot_loader_cfi.srec
 export QUARTUS_ROOTDIR=$ALTERA/quartus
 
 $NIOS_BIN/elf2flash --base=0x0 --input=vmlinux --end=0xffffffff --reset=0x0 --boot=$NIOS_BOOT
+
+if [ 0 != $? ] ; then
+    echo  FATAL ERROR : elf2flash reported an error
+    exit 1
+fi
+
 $NIOS_BIN_GNU/nios2-elf-objcopy -O ihex vmlinux.flash vmlinux.hex
 
+if [ 0 != $? ] ; then
+    echo  ERROR : nios2-elf-objcopy reported an error
+    exit 2
+fi
 
+echo  OK : vmlinux.hex created
 
